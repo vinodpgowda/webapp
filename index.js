1ef55fc0300dc586f0e7a59311477261963472ed
Vinod Kumar <vinodkumarpgowda@gmail.com>
2025-08-03 16:56:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+    res.send("Hi there!");
 });
 
 app.listen(8080, () => {
